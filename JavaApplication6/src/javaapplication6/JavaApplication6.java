13c7be5a46f061c3fce1d1a3cb1e87c14b395a5d
anas008 <anaslaflorida@gmail.com>
2016-10-11 16:35:34 +0200

    Update JavaApplication6.java

--- JavaApplication6/src/javaapplication6/JavaApplication6.java
+++ JavaApplication6/src/javaapplication6/JavaApplication6.java
@@ -16,6 +16,6 @@ public class JavaApplication6 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Goodbye");
+        System.out.println("Bye");
     }
 }
